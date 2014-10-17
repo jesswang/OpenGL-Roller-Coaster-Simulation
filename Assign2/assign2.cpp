8ef40d1f7cd776be12ecbe930872b500e23a5932
Jessica Wang <wang336@usc.edu>
2014-10-17 02:18:27 -0700

    add code comments

--- Assign2/assign2.cpp
+++ Assign2/assign2.cpp
@@ -9,6 +9,7 @@
     #include <OpenGL/glu.h>
     #include <GLUT/glut.h>
     #include <vector>
+    #include <sstream>
     #include "pic.h"
     #include "math.h"
 
@@ -39,25 +40,73 @@
     //keeps track of index in spline for moving camera for first person view in display()
     int u = 0;
 
-    int g_vMousePos[2] = {0, 0};
-    int g_iLeftMouseButton = 0;    /* 1 if pressed, 0 if not */
-    int g_iMiddleMouseButton = 0;
-    int g_iRightMouseButton = 0;
-
-    typedef enum { ROTATE, TRANSLATE, SCALE } CONTROLSTATE;
-
-    CONTROLSTATE g_ControlState = ROTATE;
-
-    /* state of the world */
-    float g_vLandRotate[3] = {0.0, 0.0, 0.0};
-    float g_vLandTranslate[3] = {0.0, 0.0, 0.0};
-    float g_vLandScale[3] = {1.0, 1.0, 1.0};
-
     double eye_x = -15.0, eye_y = -15.0, eye_z = 0.0, center_x = 0.0, center_y = 0.0, center_z = 0.0, up_x = 0.0, up_y = 0.0, up_z = 1.0;
 
     GLuint textureSky[1];
 
-    void texload(int i, char *filename) 
+    /* Write a screenshot to the specified filename */
+    void saveScreenshot (char *filename){
+        int i, j;
+        
+        Pic *in = NULL;
+        Pic *out = NULL;
+        
+        if (filename == NULL)
+            return;
+        
+        in = pic_alloc(640, 480, 3, NULL);
+        out = pic_alloc(640, 480, 3, NULL);
+        printf("File to save to: %s\n", filename);
+        
+        glReadPixels(0, 0, 640, 480, GL_RGB, GL_UNSIGNED_BYTE, &in->pix[0]);
+        
+        for ( int j=0; j<480; j++ ) {
+            for ( int i=0; i<640; i++ ) {
+                PIC_PIXEL(out, i, j, 0) = PIC_PIXEL(in, i, 480-1-j, 0);
+                PIC_PIXEL(out, i, j, 1) = PIC_PIXEL(in, i, 480-1-j, 1);
+                PIC_PIXEL(out, i, j, 2) = PIC_PIXEL(in, i, 480-1-j, 2);
+            }
+        }
+        
+        if (jpeg_write(filename, out))
+            printf("File saved Successfully\n");
+        
+        else
+            printf("Error in Saving\n");
+        
+        pic_free(in);
+        pic_free(out);
+        
+    }
+
+    void takeScreenshots(int num)
+    {
+        std::stringstream s;
+        if (num < 100)
+        {
+            if (num < 10)
+            {
+                s << "screenshots/00" << num << ".jpg";
+            }
+            else
+            {
+                s << "screenshots/0" << num << ".jpg";
+            }
+        }
+        else
+        {
+            s << "screenshots/" << num << ".jpg";
+        }
+        char* file = new char[19];
+        strcpy(file, s.str().c_str());
+        saveScreenshot(file);
+        delete [] file;
+        
+        ++num;
+        glutTimerFunc(100, takeScreenshots, num); // callback is triggered every 500 ms
+    }
+
+    void texload(int i, char *filename)
     { 
         Pic* img; 
         img = jpeg_read(filename, NULL); 
@@ -195,12 +244,11 @@
         return p;
     }
 
-    void drawSplines()
+    void computeSpline()
     {
         spline_points.clear();
         tangent_vectors.clear();
         normal_vectors.clear();
-        //glBegin(GL_LINE_STRIP);
         for(int i = 0; i < g_iNumOfSplines; ++i)
         {
             int index = 0;
@@ -213,11 +261,13 @@
                 struct point* tangent_vec = (struct point*) malloc(sizeof(struct point));
                 struct point* normal_vec = (struct point*) malloc(sizeof(struct point));
 
+                //compute spline point
                 catmull_vec->x = catmullRomPoint(g_Splines[i].points[j-1].x, g_Splines[i].points[j].x, g_Splines[i].points[j+1].x, g_Splines[i].points[j+2].x, u);
                 catmull_vec->y = catmullRomPoint(g_Splines[i].points[j-1].y, g_Splines[i].points[j].y, g_Splines[i].points[j+1].y, g_Splines[i].points[j+2].y, u);
                 catmull_vec->z = catmullRomPoint(g_Splines[i].points[j-1].z, g_Splines[i].points[j].z, g_Splines[i].points[j+1].z, g_Splines[i].points[j+2].z, u);
                 spline_points.push_back(catmull_vec);
 
+                //compute tangent and normalize it
                 tangent_vec->x = computeTangent(g_Splines[i].points[j-1].x, g_Splines[i].points[j].x, g_Splines[i].points[j+1].x, g_Splines[i].points[j+2].x, u);
                 tangent_vec->y = computeTangent(g_Splines[i].points[j-1].y, g_Splines[i].points[j].y, g_Splines[i].points[j+1].y, g_Splines[i].points[j+2].y, u);
                 tangent_vec->z = computeTangent(g_Splines[i].points[j-1].z, g_Splines[i].points[j].z, g_Splines[i].points[j+1].z, g_Splines[i].points[j+2].z, u);
@@ -239,20 +289,17 @@
                 //compute binormal and normalize it
                 binormal_vec = unit(computeCrossProduct(tangent_vec, normal_vec));
                 binormal_vectors.push_back(binormal_vec);
-
-                //glVertex3d(catmull_vec->x, catmull_vec->y, catmull_vec->z);
                 
                 ++index;
               }
             }
         }
-        //glEnd();
     }
 
     void drawRail()
     {
         glBegin(GL_QUADS);
-        for(int i = 0; i < spline_points.size() - 100; i+=100)
+        for(int i = 0; i < spline_points.size() - 200; i+=200)
         {
             struct point* v0 = (struct point*) malloc(sizeof(struct point));
             struct point* v1 = (struct point*) malloc(sizeof(struct point));
@@ -268,10 +315,10 @@
             v2 = add(spline_points[i], mult(add(mult(normal_vectors[i], -1), binormal_vectors[i]), 0.03));
             v3 = add(spline_points[i], mult(sub(mult(normal_vectors[i], -1), binormal_vectors[i]), 0.03));
         
-            v4 = add(spline_points[i+100], mult(sub(normal_vectors[i+100], binormal_vectors[i+100]), 0.03));
-            v5 = add(spline_points[i+100], mult(add(normal_vectors[i+100], binormal_vectors[i+100]), 0.03));
-            v6 = add(spline_points[i+100], mult(add(mult(normal_vectors[i+100], -1), binormal_vectors[i+100]), 0.03));
-            v7 = add(spline_points[i+100], mult(sub(mult(normal_vectors[i+100], -1), binormal_vectors[i+100]), 0.03));
+            v4 = add(spline_points[i+200], mult(sub(normal_vectors[i+200], binormal_vectors[i+200]), 0.03));
+            v5 = add(spline_points[i+200], mult(add(normal_vectors[i+200], binormal_vectors[i+200]), 0.03));
+            v6 = add(spline_points[i+200], mult(add(mult(normal_vectors[i+200], -1), binormal_vectors[i+200]), 0.03));
+            v7 = add(spline_points[i+200], mult(sub(mult(normal_vectors[i+200], -1), binormal_vectors[i+200]), 0.03));
             
             //first rail
             //front
@@ -388,8 +435,6 @@
                 glVertex3d(v4->x+0.2, v4->y+0.2, v4->z+0.2);
                 glVertex3d(v0->x+0.2, v0->y+0.2, v0->z+0.2);
             }
-            
-            
             free(v0); free(v1); free(v2); free(v3); free(v4); free(v5); free(v6); free(v7);
         }
         glEnd();
@@ -459,11 +504,10 @@
         glClearColor(0.0, 0.0, 0.0, 0.0);   
         glMatrixMode(GL_PROJECTION);
         glLoadIdentity();
-        gluPerspective(100.0, 640.0/480.0, 0.01, 1000.0);
+        gluPerspective(80.0, 640.0/480.0, 0.03, 1000.0);
         glMatrixMode(GL_MODELVIEW);
         glLoadIdentity();
-        //gluLookAt(-15.0, -15.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0);
-        glEnable(GL_DEPTH_TEST);  
+        glEnable(GL_DEPTH_TEST);
         glLineWidth(2.0);        
         glGenTextures(1, textureSky);
     }
@@ -472,19 +516,19 @@
     {
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         glPushMatrix();
+        //move the camera to simulate first person viewpoint of roller coaster ride
         if(!spline_points.empty())
         {
             gluLookAt(spline_points[u]->x, spline_points[u]->y, spline_points[u]->z, spline_points[u]->x + tangent_vectors[u]->x, spline_points[u]->y + tangent_vectors[u]->y, spline_points[u]->z + tangent_vectors[u]->z, normal_vectors[u]->x, normal_vectors[u]->y, normal_vectors[u]->z);
-            /*gluLookAt(spline_points[u]->x, spline_points[u]->y, spline_points[u]->z, spline_points[u+1]->x, spline_points[u+1]->y, spline_points[u+1]->z, 0,0,1);*/
-            if(u < spline_points.size() - 80)
+            if(u < spline_points.size() - 70)
             {
-                u+=80;
+                u+=70;
             }
             else{
                 u = 0;
             }
         }
-        drawSplines();
+        computeSpline();
         drawRail();
         glTexEnvf(GL_TEXTURE_ENV,GL_TEXTURE_ENV_MODE,GL_MODULATE); 
         glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR); 
@@ -499,84 +543,6 @@
         glutSwapBuffers();
     }
 
-    /* converts mouse drags into information about
-    rotation/translation/scaling */
-    void mousedrag(int x, int y)
-    {
-        int vMouseDelta[2] = {x-g_vMousePos[0], y-g_vMousePos[1]};
-        switch (g_ControlState)
-        {
-            case TRANSLATE:
-                if (g_iLeftMouseButton)
-                {
-                    g_vLandTranslate[0] += vMouseDelta[0]*0.01;
-                    g_vLandTranslate[1] -= vMouseDelta[1]*0.01;
-                    glTranslatef(g_vLandTranslate[0], g_vLandTranslate[1], 0.0); // translate w/ respect to x- and y- axes
-                }
-                if (g_iMiddleMouseButton)
-                {
-                    g_vLandTranslate[2] += vMouseDelta[1]*0.01;
-                    glTranslatef(0.0, 0.0, g_vLandTranslate[2]); // translate w/ respect to z-axis
-                }
-                break;
-            case ROTATE:
-                if (g_iLeftMouseButton)
-                {
-                    g_vLandRotate[0] += vMouseDelta[1]*0.01;
-                    g_vLandRotate[1] += vMouseDelta[0]*0.01;
-                    glRotatef(g_vLandRotate[0], 1.0, 0.0, 0.0); // rotate about x-axis
-                    glRotatef(g_vLandRotate[1], 0.0, 1.0, 0.0); // rotate about y-axis
-                }
-                if (g_iMiddleMouseButton)
-                {
-                    g_vLandRotate[2] += vMouseDelta[1]*0.01;
-                    glRotatef(g_vLandRotate[2], 0.0, 0.0, 1.0); // rotate about z-axis
-                }
-                break;
-        }
-            g_vMousePos[0] = x;
-            g_vMousePos[1] = y;
-    }
-
-    void mouseidle(int x, int y)
-    {
-        g_vMousePos[0] = x;
-        g_vMousePos[1] = y;
-    }
-
-    void mousebutton(int button, int state, int x, int y)
-    {
-
-        switch (button)
-        {
-            case GLUT_LEFT_BUTTON:
-                g_iLeftMouseButton = (state==GLUT_DOWN);
-                break;
-            case GLUT_MIDDLE_BUTTON:
-                g_iMiddleMouseButton = (state==GLUT_DOWN);
-                break;
-            case GLUT_RIGHT_BUTTON:
-                g_iRightMouseButton = (state==GLUT_DOWN);
-                break;
-        }
-
-        switch(glutGetModifiers())
-        {
-            case GLUT_ACTIVE_CTRL:
-                g_ControlState = TRANSLATE;
-                break;
-            case GLUT_ACTIVE_SHIFT:
-                g_ControlState = SCALE;
-                break;
-            default:
-                g_ControlState = ROTATE;
-                break;
-        }
-
-        g_vMousePos[0] = x;
-        g_vMousePos[1] = y;
-    }
-
     void doIdle()
     {
         /* make the screen update */
@@ -588,6 +554,10 @@
         if(c == 27)
         {
             exit(0);
+        }
+        else if(c == 's')
+        {
+            takeScreenshots(0);
         }
     }
 
@@ -611,9 +581,6 @@
         glutDisplayFunc(display);
         glutIdleFunc(doIdle);
         glutKeyboardFunc(keyboard);
-        glutMotionFunc(mousedrag);
-        glutPassiveMotionFunc(mouseidle);
-        glutMouseFunc(mousebutton);
         glutMainLoop();
 
         return 0;
